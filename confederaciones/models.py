7d50f477fe9d48fa042972c7f51a6d95d343174d
Alberto Madrazo <betomadrazo@gmail.com>
2017-06-24 12:52:14 -0500

    Crea development.py para guardar la informacion para desarrollo.

--- confederaciones/models.py
+++ confederaciones/models.py
@@ -1,8 +1,37 @@
 from django.db import models
 
+# class Grupo(models.Model):
+#     grupo = models.CharField(max_length=1)
+
+#     def creaPartidosRonda(self, selecciones):
+#         # Con el array de Selecciones crear los partidos entre ellas
+#         juegos = []
+
+#         while selecciones:
+#             for i in selecciones:
+#                 for j in selecciones:
+#                     if i != j:
+#                         juegos.append((i, j,))
+#                 selecciones.remove(i)
+#                 break
+
+#         return juegos
+
+#     def __str__(self):
+#         return self.grupo
+
 
 class Seleccion(models.Model):
+    # grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200)
+    # escudo = models.ImageField(upload_to='banderas', blank=True, null=True)
 
     def __str__(self):
         return self.nombre
+
+
+# class Juego(models.Model):
+#     local = Seleccion()
+#     visita = Seleccion()
+#     resultado = IntegerField()
+#     goles = 
--- ruski/settings/base.py
+++ ruski/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')@*-%5o6t$29y#na5*@mc5tj-7bu_8z(iu!r14mm76&)0py%ko'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,14 +72,6 @@ WSGI_APPLICATION = 'ruski.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -121,3 +113,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
